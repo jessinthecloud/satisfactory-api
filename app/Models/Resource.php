8a0ee198fd4410221743eee185423179a8aa8748
jessinthecloud <code@jessinthe.cloud>
2022-09-23 16:56:16 -0400

    unguard attrs

--- app/Models/Resource.php
+++ app/Models/Resource.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Resource extends Model
 {
     use HasFactory;
+    
+    protected $guarded = ['id'];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'type' => \App\Enums\ResourceType::class,
+    ];
 }
