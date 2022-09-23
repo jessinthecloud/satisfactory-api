57bfedf12370da998a15388f43fffd98d43d7170
jessinthecloud <code@jessinthe.cloud>
2022-09-23 18:15:01 -0400

    tester dump

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    dump(json_decode(Storage::disk('public')->get('data.json')));
+    dump(collect(json_decode(Storage::disk('public')->get('data.json'))->recipes)->all());
 });
