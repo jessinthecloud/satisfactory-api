02337b8425f1c4cfee7c632d339a6f9d058be1ce
jess <code@jessinthe.cloud>
2022-09-24 15:16:57 -0400

    Update nightmare

--- routes/web.php
+++ routes/web.php
@@ -17,97 +17,71 @@ use \ForceUTF8\Encoding;
 Route::get('/', function () {
 //    dump(json_decode(Storage::disk('public')->get('data.json')));
 //    Cache::flush();
-    /*$data = Cache::remember('raw-game-data', 300, function () {
-        return collect(json_decode(Storage::disk('public')->get('Docs.json')));
+    $data = Cache::remember('raw-game-data', 300, function () {
+//        $raw_data = file_get_contents('/var/www/html/storage/app/public/Docs-fixed.json');
+//        $json_data = json_decode($raw_data);
+        return collect(json_decode(Storage::disk('public')->get('Docs-fixed.json')));
     });
-
-    dump($data->all());*/
-//    dump(json_decode(Storage::disk('public')->get('Docs.pretty.json')));
-
-/*    $raw_data = '[
-  {
-    "NativeClass": "Class\'/Script/FactoryGame.FGItemDescriptor\'",
-    "Classes": [
-      {
-        "mFluidColor": "(B\u003d0,G\u003d0,R\u003d0,A\u003d0)",
-        "mGasColor": "(B\u003d0,G\u003d0,R\u003d0,A\u003d0)"
-      }
-      ]
+//    dump(array_values($data->pluck('NativeClass')->map(function($item, $key){
+//        return Str::remove('Class\'/Script/FactoryGame.FG', $item);
+//    })->sort()->all()));
+    
+    $data->transform(function($item, $key){
+        $fqcn = Str::between($item->NativeClass, "Class'", "'");
+        $item->fqcn = $fqcn;
+        $class_name = Str::remove('/Script/FactoryGame.FG', $fqcn);
+        $item->shortClass = $class_name;
+        return $item;
+    });
+    
+//    dd($data->first());
+    
+    $native_class_names = $data->pluck('NativeClass')->sort()->all();
+    // Native classes
+    echo "<table>";
+    echo "<tr><th>Name</th><th>FQCN</th></tr>";
+    foreach($native_class_names as $class_name_string){
+        $fqcn = Str::between($class_name_string, "Class'", "'");
+        $class_name = Str::remove('/Script/FactoryGame.FG', $fqcn);
+//        $class_name = implode(' ', Str::ucsplit($class_name));
+        echo "<tr><td>$class_name</td><td>$fqcn</td></tr>";
     }
-]';
+    echo "</table>";
 
-    echo "Detected encoding: "; var_dump(mb_detect_encoding($raw_data, null, strict: true));
-    echo "<BR><HR><BR>"; 
-    
-    print_r(json_decode(
-        $raw_data,
-        null,
-        512,
-        JSON_INVALID_UTF8_IGNORE | JSON_PARTIAL_OUTPUT_ON_ERROR 
-    ));*/
-    
-    // don't replace invalid chars with < ? > 
-//    ini_set('mbstring.substitute_character', 'none');
-    
-    if (!function_exists('mb_str_replace'))
-{
-   function mb_str_replace($search, $replace, $subject, &$count = 0)
-   {
-      if (!is_array($subject))
-      {
-         $searches = is_array($search) ? array_values($search) : array($search);
-         $replacements = is_array($replace) ? array_values($replace) : array($replace);
-         $replacements = array_pad($replacements, count($searches), '');
-         foreach ($searches as $key => $search)
-         {
-            $parts = mb_split(preg_quote($search), $subject);
-            $count += count($parts) - 1;
-            $subject = implode($replacements[$key], $parts);
-         }
-      }
-      else
-      {
-         foreach ($subject as $key => $value)
-         {
-            $subject[$key] = mb_str_replace($search, $replace, $value, $count);
-         }
-      }
-      return $subject;
-   }
-}
-    
-//     $raw_data = Storage::disk('public')->get('Docs.pretty.json');
-     $raw_data = file_get_contents('/var/www/html/storage/app/public/Docs-fixed.json');
-//     $raw_data = mb_str_replace('™', '', $raw_data);
-     
-//     var_dump(Str::between($raw_data, '[ {', '} ] } ]'));
-     
-//     $raw_data = trim(Str::between($raw_data, '[ {', '} ] } ]'));
-//     print_r($raw_data);
-//     echo "<BR><BR>";
-//    die;
+//    dump($data->first());
+    $important_classes = [
+        "Recipe",
+        "BuildableManufacturer",
+        "BuildableFrackingExtractor"
+    ];
+//    dump($data->filter(function ($value, $key) use ($important_classes) {
+//        return Str::contains($value->NativeClass, $important_classes, true);
+//    })->all());
+    echo "<ul style='list-style: none;'>";
+    foreach($data as $data_row){
+        if(!in_array($data_row->shortClass, $important_classes)){
+            continue;
+        }
+        echo "<li><strong>{$data_row->fqcn} : </strong><BR>";
+//            dump(array_column($data_row->Classes, 'ClassName'));
+            echo "<ul>";
+                foreach($data_row->Classes as $class){
+//                var_dump($class);
+                    echo "<li>{$class->ClassName} <BR>";
+                    $properties = array_keys(get_object_vars($class));
+                    if($data_row->shortClass !== 'Recipe'){
+                        dump($properties);
+//                        dump($class);
+                    } 
+                    echo "<BR></li>";
+                }
+            echo "</ul>";
+        echo "</li>";
+    }
+    echo "</ul>";
 
-//     $raw_data = Encoding::toUTF8($raw_data);
-//     $raw_data = trim(Encoding::fixUTF8($raw_data, Encoding::ICONV_IGNORE));
-//     $raw_data = Encoding::fixUTF8($raw_data, Encoding::ICONV_TRANSLIT);
-//     $raw_data = mb_str_replace('Factory Cart"!"', 'Factory Cart"', $raw_data);
-//     $raw_data = mb_str_replace('"Fuel:', '"Fuel ', $raw_data);
-    
-//     echo "Detected encoding: "; var_dump(mb_detect_encoding($raw_data, null, strict: true));
-     
-//     echo "<BR><BR>";
-     
-//     print_r($raw_data);
-     
-//    echo "<BR><HR><BR>";
-
-    $json_data = json_decode(
-        $raw_data,
-//        null,
-//        512,
-//        JSON_INVALID_UTF8_IGNORE | JSON_PARTIAL_OUTPUT_ON_ERROR | JSON_THROW_ON_ERROR
-//        JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_IGNORE | JSON_UNESCAPED_LINE_TERMINATORS | JSON_PARTIAL_OUTPUT_ON_ERROR 
-    );
+//    $raw_data = file_get_contents('/var/www/html/storage/app/public/Docs-fixed.json');
+//    $json_data = json_decode($raw_data);
     /*echo match (json_last_error()) {
         JSON_ERROR_NONE => ' - No errors',
         JSON_ERROR_DEPTH => ' - Maximum stack depth exceeded',
@@ -118,9 +92,9 @@ Route::get('/', function () {
         default => ' - Unknown error',
     };*/
 //    echo "<BR><HR><BR>";
-    print_r($json_data);
-//    print_r(htmlspecialchars(file_get_contents('/var/www/html/storage/app/public/Docs.pretty.json'), ENT_IGNORE));
-//    print_r(json_decode(utf8_encode(trim(Storage::disk('public')->get('Docs.json')))));
+//    dump($json_data);
+//    print_r($json_data);
+
     /*$data->each(function ($item, $key) {
         
     });*/
